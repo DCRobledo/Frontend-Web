9b3365b63a5f0f385991a911bfdbcc0861d13b07
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-02-13 12:08:28 +0100

    Created hardcoded-data version of Entry component. Also created Header component

---
+++ 02_TravelJournal/src/components/Header.jsx
@@ -0,0 +1,10 @@
+﻿const Header = () => {
+    return (
+        <header>
+            <img src={"globe.png"} alt="World Globe"/>
+            <p>my travel journal.</p>
+        </header>
+    );
+};
+
+export default Header;
