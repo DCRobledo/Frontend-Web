57e31ca2525a0bdc8973aedc1d8dab6d733c6442
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-02-13 12:43:13 +0100

    Parametrized data loading from data.js

--- 02_TravelJournal/src/App.jsx
+++ 02_TravelJournal/src/App.jsx
@@ -1,14 +1,24 @@
 import Header from "./components/Header.jsx";
 import Entry from "./components/Entry.jsx";
 
+import data from "./data.js";
+
 const App = () => {
+    const entryElements = data.map((item) => {
+        return (
+            <Entry 
+                key={item.id}
+                {...item}
+            />
+        )
+    })
+    
     return (
         <>
             <Header />
-            <ul className={"entry-list"}>
-                <Entry />
-            </ul>
-            
+            <main className={"entry-list"}>
+                {entryElements}
+            </main>
         </>
     );
 };
--- 02_TravelJournal/src/components/Entry.jsx
+++ 02_TravelJournal/src/components/Entry.jsx
@@ -1,25 +1,21 @@
-﻿const Entry = () => {
+﻿const Entry = (data) => {
     return (
         <>
-            <div className={"entry"}>
-                <img src={"japan.png"} alt={"Destiny Image"} className={"entry-destiny-image"}/>
+            <article className={"entry"}>
+                <img src={data.img?.src} alt={data.img?.alt} className={"entry-destiny-image"}/>
                 <div className={"entry-data-container"}>
                     <div className={"entry-data-container-location"}>
                         <img src={"marker.png"} alt={"Marker Icon"} className={"entry-marker"}/>
-                        <p>JAPAN</p>
-                        <a href={"https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"}>
+                        <p>{data.country}</p>
+                        <a href={data.googleMapsLink}>
                             View on Google Maps
                         </a>
                     </div>
-                    <p className={"entry-data-destiny"}>Mount Fuji</p>
-                    <p className={"entry-data-date"}>12 Jan, 2021 - 24 Jan, 2021</p>
-                    <p className={"entry-data-description"}>Mount Fuji is the tallest mountain in Japan, standing at
-                        3,776
-                        meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both
-                        Japanese
-                        and foreign tourists.</p>
+                    <p className={"entry-data-destiny"}>{data.title}</p>
+                    <p className={"entry-data-date"}>{data.dates}</p>
+                    <p className={"entry-data-description"}>{data.text}</p>
                 </div>
-            </div>
+            </article>
             <hr className={"entry-horizontal-bar"} />
         </>
     );
