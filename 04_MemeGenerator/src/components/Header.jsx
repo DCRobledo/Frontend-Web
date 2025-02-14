43c76a16a45b83a889da2e1ec7d124af1b2a1a8c
Daniel Robledo <daniel.canorobledo@gmail.com>
2025-02-14 13:48:17 +0100

    Implemented side effect for meme's text

--- 04_MemeGenerator/src/components/Header.jsx
+++ 04_MemeGenerator/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header className="header">
-            <img src={"troll-face.png"}/>
+            <img src={"troll-face.png"} alt={"Troll Face"}/>
             <h1>Meme Generator</h1>
         </header>
     )
